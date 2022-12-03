a92359742789e49bf8a5e3425068bf8bd8387b5c
848774411 <119307886+848774411@users.noreply.github.com>
2022-12-03 09:18:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'eR6dhkg_CAV_Jc2VukFrvIq45OwDDoNpuKHQ2d1qDns',
+  CALLBACK_TEMPLATE_ID: 'MK110W_uODDZrRqiY5eAj3vtqpVvN2tab0PK4E7D7rE',
 
   CALLBACK_USERS: [
     {
